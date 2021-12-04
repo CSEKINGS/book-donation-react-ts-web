d25f2a896ac60bd035de41719b389bc09c6a0fa7
SivaPrakasam7 <prakasams22@gmail.com>
2021-12-04 21:39:03 +0530

    layout image updated

--- src/app/layouts/Account/main.tsx
+++ src/app/layouts/Account/main.tsx
@@ -1,22 +1,25 @@
 import * as Mui from "@mui/material";
 import * as MuiIcons from "@mui/icons-material";
 import * as Router from "react-router-dom";
+import * as Assets from "src/assets";
 
 export const Main = () => (
   <Mui.Grid container justifyContent="center" alignItems="center">
-    <Mui.Grid item xs={12} md={6}>
+    <Mui.Grid item xs={12} md={5}>
       <Mui.Container maxWidth="xs" sx={{ my: { xs: 5, md: 1 } }}>
         <MuiIcons.AutoAwesome color="primary" />
         <Router.Outlet />
       </Mui.Container>
     </Mui.Grid>
-    <Mui.Grid item xs={6} sx={{ display: { xs: "none", md: "flex" } }}>
-      <Mui.Box
+    <Mui.Grid item xs={7} sx={{ display: { xs: "none", md: "flex" } }}>
+      <Mui.CardMedia
+        component="img"
+        image={Assets.landingImages.AccountImage}
         sx={{
-          flexGrow: 1,
-          bgcolor: (theme) => theme.palette.primary.main,
           minHeight: "100vh",
-          height: "100%",
+          borderRadius: 20,
+          borderTopRightRadius: 0,
+          borderBottomRightRadius: 0,
         }}
       />
     </Mui.Grid>
--- src/assets/main.ts
+++ src/assets/main.ts
@@ -1,4 +1,7 @@
+import AccountImage from "src/assets/landing-images/syllabus.jpg";
+
 export const landingImages = {
   AccountBanner:
     "https://media.gettyimages.com/vectors/donation-box-full-of-toys-books-vector-id1196919116",
+  AccountImage,
 };
