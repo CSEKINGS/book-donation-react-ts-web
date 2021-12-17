ad6a59c4c99c77367ae8f08fbc4f0ba83dd908a2
SivaPrakasam7 <prakasams22@gmail.com>
2021-12-17 17:25:22 +0530

    View map added

--- src/app/pages/books/dialogs/locate.tsx
+++ src/app/pages/books/dialogs/locate.tsx
@@ -1,4 +1,5 @@
 import * as Mui from "@mui/material";
+import * as MuiIcons from "@mui/icons-material";
 import * as Router from "react-router-dom";
 import * as Components from "src/app/components";
 
@@ -9,7 +10,7 @@ export const Locate = () => {
     },
   } = Router.useLocation();
   return (
-    <Components.Dialog open={true} title="locate" onConfirm={() => {}}>
+    <Components.Dialog open={true} title="locate" fullScreen={false}>
       <Mui.DialogContent>
         <Mui.Stack alignItems="center">
           <Mui.Avatar
@@ -18,9 +19,16 @@ export const Locate = () => {
             sx={{ height: { xs: 200, md: 150 }, width: "100%" }}
           />
           <Mui.Typography variant="h6">{name}</Mui.Typography>
-          <Mui.Typography variant="h6">
-            Geo Location : {location.join(", ")}
-          </Mui.Typography>
+          <Mui.Button
+            // variant="outlined"
+            size="large"
+            component="a"
+            target="_blank"
+            href={`https://maps.google.com/?q=${location.join(", ")}`}
+            startIcon={<MuiIcons.LocationOn fontSize="inherit" />}
+          >
+            View Map
+          </Mui.Button>
         </Mui.Stack>
       </Mui.DialogContent>
     </Components.Dialog>
