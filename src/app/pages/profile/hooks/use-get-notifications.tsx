fba4443624d414fdd2f96e3be29494a52fc18222
SivaPrakasam7 <prakasams22@gmail.com>
2021-12-17 11:12:16 +0530

    notification socket updated

--- src/app/pages/profile/hooks/use-get-notifications.tsx
+++ src/app/pages/profile/hooks/use-get-notifications.tsx
@@ -1,13 +1,21 @@
 import * as Pages from "src/app/pages";
 import * as Hooks from "src/app/hooks";
 import * as Api from "src/api";
+import * as Socket from "src/socket";
+import * as React from "react";
 
 export const useGetNotifications = (): Notifications.Type => {
   const { user } = Hooks.useSignInCheck();
+  const [notification, setNotifications] = React.useState("");
+  Socket.socket.on("notifications", (msg) => {
+    console.log(msg);
+    setNotifications(msg);
+  });
   const { data, isFetching } = Api.Server.useRequest(
-    ["notifications", user?._id as string],
+    ["notifications", user?._id as string, notification],
     "notification"
   );
+  console.log(notification);
   return { notifications: data, loading: isFetching };
 };
 
